4b5fbb513713ce043ed7655c13e172bb7b795f1b
Max Wittal <wittal@neobotix.de>
2020-04-20 16:43:54 +0200

    added 2D/1D/0D detection and constrain feature

--- src/neo_localization_node.cpp
+++ src/neo_localization_node.cpp
@@ -24,6 +24,8 @@
 #include <mutex>
 #include <thread>
 #include <random>
+#include <cmath>
+#include <array>
 
 
 template<typename T>
@@ -82,6 +84,94 @@ Matrix<double, 4, 4> convert_transform_3(const tf::Transform& trans)
 	return res;
 }
 
+template<typename T>
+T compute_variance(const std::vector<double>& values, T& mean)
+{
+	if(values.size() < 2) {
+		throw std::logic_error("values.size() < 2");
+	}
+	mean = 0;
+	for(auto v : values) {
+		mean += v;
+	}
+	mean /= T(values.size());
+
+	double var = 0;
+	for(auto v : values) {
+		var += std::pow(v - mean, 2);
+	}
+	var /= T(values.size() - 1);
+	return var;
+}
+
+template<typename T, size_t N>
+Matrix<T, 2, 2> compute_covariance_2(const std::vector<Matrix<T, N, 1>>& points, Matrix<T, 2, 1>& mean)
+{
+	if(N < 2) {
+		throw std::logic_error("N < 2");
+	}
+	if(points.size() < 2) {
+		throw std::logic_error("points.size() < 2");
+	}
+	mean = Matrix<T, 2, 1>();
+	for(auto point : points) {
+		mean += Matrix<T, 2, 1>{point[0], point[1]};
+	}
+	mean /= T(points.size());
+
+	Matrix<T, 2, 2> mat;
+	for(auto point : points) {
+		mat(0, 0) += std::pow(point[0] - mean[0], 2);
+		mat(1, 1) += std::pow(point[1] - mean[1], 2);
+		mat(1, 0) += (point[0] - mean[0]) * (point[1] - mean[1]);
+	}
+	mat(0, 1) = mat(1, 0);
+	mat /= T(points.size() - 1);
+	return mat;
+}
+
+template<typename T, size_t N>
+T compute_variance_along_direction_2(	const std::vector<Matrix<T, N, 1>>& points,
+										const Matrix<T, 2, 1>& mean,
+										const Matrix<T, 2, 1>& direction)
+{
+	if(N < 2) {
+		throw std::logic_error("N < 2");
+	}
+	if(points.size() < 2) {
+		throw std::logic_error("points.size() < 2");
+	}
+	double var = 0;
+	for(auto point : points)
+	{
+		const auto delta = Matrix<T, 2, 1>{point[0], point[1]} - mean;
+		var += std::pow(direction.dot(delta), 2);
+	}
+	var /= T(points.size() - 1);
+	return var;
+}
+
+// See: http://croninprojects.org/Vince/Geodesy/FindingEigenvectors.pdf
+// See: https://math.stackexchange.com/questions/395698/fast-way-to-calculate-eigen-of-2x2-matrix-using-a-formula
+template<typename T>
+Matrix<T, 2, 1> compute_eigenvectors_2(	const Matrix<T, 2, 2>& mat,
+										std::array<Matrix<T, 2, 1>, 2>& eigen_vectors)
+{
+	Matrix<T, 2, 1> eigen_values;
+	const T tmp_0 = std::sqrt(std::pow(mat(0, 0) + mat(1, 1), T(2)) - T(4) * (mat(0, 0) * mat(1, 1) - mat(1, 0) * mat(0, 1)));
+	eigen_values[0] = (mat(0, 0) + mat(1, 1) + tmp_0) / T(2);
+	eigen_values[1] = (mat(0, 0) + mat(1, 1) - tmp_0) / T(2);
+	if(eigen_values[1] > eigen_values[0]) {
+		std::swap(eigen_values[0], eigen_values[1]);
+	}
+
+	eigen_vectors[0] = Matrix<T, 2, 1>{mat(0, 1), eigen_values[0] - mat(0, 0)};
+	eigen_vectors[1] = Matrix<T, 2, 1>{eigen_values[1] - mat(1, 1), mat(1, 0)};
+	eigen_vectors[0] /= eigen_vectors[0].norm();
+	eigen_vectors[1] /= eigen_vectors[1].norm();
+	return eigen_values;
+}
+
 
 class NeoLocalizationNode {
 public:
@@ -102,9 +192,10 @@ public:
 		m_node_handle.param("update_gain", m_update_gain, 0.5);
 		m_node_handle.param("confidence_gain", m_confidence_gain, 0.01);
 		m_node_handle.param("max_confidence", m_max_confidence, 0.95);
-		m_node_handle.param("sample_std_x", m_sample_std_x, 0.5);
-		m_node_handle.param("sample_std_y", m_sample_std_y, 0.5);
+		m_node_handle.param("sample_std_xy", m_sample_std_xy, 0.5);
 		m_node_handle.param("sample_std_yaw", m_sample_std_yaw, 0.5);
+		m_node_handle.param("constrain_threshold", m_constrain_threshold, 100.);
+		m_node_handle.param("disable_threshold", m_disable_threshold, 1.);
 
 		m_sub_scan_topic = m_node_handle.subscribe("/scan", 10, &NeoLocalizationNode::scan_callback, this);
 		m_sub_map_topic = m_node_handle.subscribe("/map", 1, &NeoLocalizationNode::map_callback, this);
@@ -153,13 +244,6 @@ protected:
 		const Matrix<double, 4, 4> L = convert_transform_25(base_to_odom);
 		const Matrix<double, 4, 4> T = translate25(m_offset_x, m_offset_y) * rotate25_z(m_offset_yaw);		// odom to map
 
-		const Matrix<double, 3, 1> grid_pose = (m_grid_to_map.inverse() * T * L * Matrix<double, 4, 1>{0, 0, 0, 1}).project();
-
-		// set initial guess to odometry prediction
-		m_solver.pose_x = grid_pose[0];
-		m_solver.pose_y = grid_pose[1];
-		m_solver.pose_yaw = grid_pose[2];
-
 		std::vector<scan_point_t> points;
 
 		for(size_t i = 0; i < scan->ranges.size(); ++i)
@@ -189,13 +273,20 @@ protected:
 		pose_array->header.stamp = scan->header.stamp;
 		pose_array->header.frame_id = "map";
 
+		// calc predicted grid pose based on odometry
+		const Matrix<double, 3, 1> grid_pose = (m_grid_to_map.inverse() * T * L * Matrix<double, 4, 1>{0, 0, 0, 1}).project();
+
 		// setup distributions
 		const double rel_std_dev = fmax(1 - m_confidence, 0);
-		std::normal_distribution<double> dist_x(m_solver.pose_x, m_sample_std_x * rel_std_dev);
-		std::normal_distribution<double> dist_y(m_solver.pose_y, m_sample_std_y * rel_std_dev);
-		std::normal_distribution<double> dist_yaw(m_solver.pose_yaw, m_sample_std_yaw * rel_std_dev);
+		std::normal_distribution<double> dist_x(grid_pose[0], m_sample_std_xy * rel_std_dev);
+		std::normal_distribution<double> dist_y(grid_pose[1], m_sample_std_xy * rel_std_dev);
+		std::normal_distribution<double> dist_yaw(grid_pose[2], m_sample_std_yaw * rel_std_dev);
 
 		// solve odometry prediction first
+		m_solver.pose_x = grid_pose[0];
+		m_solver.pose_y = grid_pose[1];
+		m_solver.pose_yaw = grid_pose[2];
+
 		for(int iter = 0; iter < m_solver_iterations; ++iter) {
 			m_solver.solve<float>(*m_map, points, 1);
 		}
@@ -205,6 +296,10 @@ protected:
 		double best_yaw = m_solver.pose_yaw;
 		double best_score = m_solver.r_norm;
 
+		std::vector<Matrix<double, 4, 1>> seeds(m_sample_rate);
+		std::vector<Matrix<double, 4, 1>> samples(m_sample_rate);
+		std::vector<double> sample_errors(m_sample_rate);
+
 		for(int i = 0; i < m_sample_rate; ++i)
 		{
 			// generate new sample
@@ -212,10 +307,17 @@ protected:
 			m_solver.pose_y = dist_y(m_generator);
 			m_solver.pose_yaw = dist_yaw(m_generator);
 
+			seeds[i] = Matrix<double, 4, 1>{m_solver.pose_x, m_solver.pose_y, m_solver.pose_yaw, 1};
+
 			// solve sample
 			for(int iter = 0; iter < m_solver_iterations; ++iter) {
 				m_solver.solve<float>(*m_map, points, 1);
 			}
+
+			// save sample
+			const auto sample = Matrix<double, 4, 1>{m_solver.pose_x, m_solver.pose_y, m_solver.pose_yaw, 1};
+			samples[i] = sample;
+			sample_errors[i] = m_solver.r_norm;
 
 			// check if sample is better
 			if(m_solver.r_norm < best_score) {
@@ -227,8 +329,7 @@ protected:
 
 			// add to visualization
 			{
-				const Matrix<double, 3, 1> map_pose = (m_grid_to_map *
-						Matrix<double, 4, 1>{m_solver.pose_x, m_solver.pose_y, m_solver.pose_yaw, 1}).project();
+				const Matrix<double, 3, 1> map_pose = (m_grid_to_map * sample).project();
 				tf::Pose pose;
 				pose.setOrigin(tf::Vector3(map_pose[0], map_pose[1], 0));
 				pose.setRotation(tf::createQuaternionFromYaw(map_pose[2]));
@@ -238,36 +339,83 @@ protected:
 			}
 		}
 
-		// use best sample
-		m_solver.pose_x = best_x;
-		m_solver.pose_y = best_y;
-		m_solver.pose_yaw = best_yaw;
+		// compute covariances
+		double mean_error = 0;
+		Matrix<double, 2, 1> mean_xy;
+		Matrix<double, 2, 1> seed_mean_xy;
+		const double var_error = compute_variance(sample_errors, mean_error);
+		const Matrix<double, 2, 2> var_xy = compute_covariance_2(samples, mean_xy);
+		const Matrix<double, 2, 2> seed_var_xy = compute_covariance_2(seeds, seed_mean_xy);
 
-		// get new pose from solver
-		const Matrix<double, 4, 4> grid_pose_new = translate25(m_solver.pose_x, m_solver.pose_y) * rotate25_z(m_solver.pose_yaw);
+		// compute "measured" error characteristic
+		std::array<Matrix<double, 2, 1>, 2> eigen_vectors;
+		const Matrix<double, 2, 1> eigen_values = compute_eigenvectors_2(var_xy, eigen_vectors);
+		const Matrix<double, 2, 1> sigma_uv {sqrt(fabs(eigen_values[0])), sqrt(fabs(eigen_values[1]))};
 
-		// compute new odom to map offset from new pose
-		const Matrix<double, 3, 1> new_offset =
-				(m_grid_to_map * grid_pose_new * L.inverse() * Matrix<double, 4, 1>{0, 0, 0, 1}).project();
+		// compute seed variance along "measured" eigen vectors
+		const double seed_std_u = sqrt(compute_variance_along_direction_2(seeds, seed_mean_xy, eigen_vectors[0]));
+		const double seed_std_v = sqrt(compute_variance_along_direction_2(seeds, seed_mean_xy, eigen_vectors[1]));
 
-		// apply new offset with an exponential low pass filter
-		const double gain_factor = double(points.size()) / scan->ranges.size();
-		const double gain = fmin(m_update_gain * gain_factor, 1);
-		m_offset_x = new_offset[0] * gain + m_offset_x * (1 - gain);
-		m_offset_y = new_offset[1] * gain + m_offset_y * (1 - gain);
-		m_offset_yaw += angles::shortest_angular_distance(m_offset_yaw, new_offset[2]) * gain;
-		m_offset_time = scan->header.stamp;
+		// decide if we have 2D, 1D or 0D localization
+		const double factor_0 = fmax(sqrt(var_error) / 0.01, 0.1);
+		const double factor_1 = sigma_uv[0] / sigma_uv[1];
+		const double factor_2 = sigma_uv[0] / seed_std_u;
+		const double factor_3 = sigma_uv[1] / seed_std_v;
 
-		// apply confidence gain
-		m_confidence += (m_max_confidence - m_confidence) * 0.01 * gain_factor;
+		int mode = -1;
+		if(factor_3 / factor_0 > m_disable_threshold) {
+			mode = 0;		// high sigma_uv plus low var_error indicates no error gradient = nothing to localize with
+		} else if(factor_2 > 0.1 && factor_1 > m_constrain_threshold) {
+			mode = 1;		// high sigma imbalance means we can only localize in one direction
+		} else {
+			mode = 2;		// all good
+		}
 
-		// publish new transform
-		broadcast();
+		if(mode > 0)
+		{
+			double new_grid_x = best_x;
+			double new_grid_y = best_y;
+
+			if(mode == 1)
+			{
+				// constrain update to the good direction (ie. in direction of the eigen vector with the smaller sigma)
+				const auto delta = Matrix<double, 2, 1>{best_x, best_y} - Matrix<double, 2, 1>{grid_pose[0], grid_pose[1]};
+				const auto dist = eigen_vectors[1].dot(delta);
+				new_grid_x = grid_pose[0] + dist * eigen_vectors[1][0];
+				new_grid_y = grid_pose[1] + dist * eigen_vectors[1][1];
+			}
+
+			// use best sample for update
+			Matrix<double, 4, 4> grid_pose_new = translate25(new_grid_x, new_grid_y) * rotate25_z(best_yaw);
+
+			// compute new odom to map offset from new grid pose
+			const Matrix<double, 3, 1> new_offset =
+					(m_grid_to_map * grid_pose_new * L.inverse() * Matrix<double, 4, 1>{0, 0, 0, 1}).project();
+
+			// apply new offset with an exponential low pass filter
+			const double gain_factor = double(points.size()) / scan->ranges.size();
+			const double gain = fmin(m_update_gain * gain_factor, 1);
+			m_offset_x += (new_offset[0] - m_offset_x) * gain;
+			m_offset_y += (new_offset[1] - m_offset_y) * gain;
+			m_offset_yaw += angles::shortest_angular_distance(m_offset_yaw, new_offset[2]) * gain;
+			m_offset_time = scan->header.stamp;
+
+			if(mode > 1) {
+				// apply confidence gain
+				m_confidence += (m_max_confidence - m_confidence) * m_confidence_gain * gain_factor;
+			} else {
+				// TODO: subtract confidence based on distance moved
+			}
+
+			// publish new transform
+			broadcast();
+		}
 
 		// publish visualization
 		m_pub_pose_array.publish(pose_array);
 
-		ROS_INFO_STREAM("NeoLocalizationNode: r_norm=" << best_score << ", gain=" << gain << ", confidence=" << m_confidence);
+		ROS_INFO_STREAM("NeoLocalizationNode: r_norm=" << best_score << ", sigma_uv=[" << sigma_uv[0] << ", " << sigma_uv[1]
+				<< "] m, confidence=" << m_confidence << ", mode=" << mode << "D");
 	}
 
 	void pose_callback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& pose)
@@ -480,9 +628,10 @@ private:
 	double m_update_gain = 0;
 	double m_confidence_gain = 0;
 	double m_max_confidence = 0;
-	double m_sample_std_x = 0;
-	double m_sample_std_y = 0;
+	double m_sample_std_xy = 0;
 	double m_sample_std_yaw = 0;
+	double m_constrain_threshold = 0;
+	double m_disable_threshold = 0;
 	double m_map_update_rate = 0;
 
 	double m_offset_x = 0;			// current x offset between odom and map
